8a66a1f03794808612cdd73377d2e366cb274bf4
just1raymond <163850744+just1raymond@users.noreply.github.com>
2024-03-27 09:19:39 +0100

    Create index.py

---
+++ index.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+import os
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+
+# Set the paths using Heroku environment variables
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
+# Your web scraping code here
+driver.get("https://example.com")
+print(driver.page_source)
+driver.quit()
+print("Finished!")
