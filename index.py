244ff31cc3ad28874b308f324cd1b4c357463915
just1raymond <163850744+just1raymond@users.noreply.github.com>
2024-03-27 09:39:19 +0100

    Update index.py

--- index.py
+++ index.py
@@ -11,7 +11,7 @@ chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 # Your web scraping code here
-driver.get("https://example.com")
+driver.get("https://www.sportybet.com/ng/m/")
 print(driver.page_source)
 driver.quit()
 print("Finished!")
